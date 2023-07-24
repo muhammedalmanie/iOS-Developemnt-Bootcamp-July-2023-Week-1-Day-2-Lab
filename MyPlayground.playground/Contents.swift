6a7080b5cbe5407c4460c7978a28016267cf375c
Mohammed <94794248+muhammedalmanie@users.noreply.github.com>
2023-07-24 22:21:54 +0300

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+// Lab 2
+
+// Task 1
+var score = 5
+score += 10
+print(score)
+
+// Task 2
+if score > 9 {
+    print("Pass")
+} else {
+    print("Fail")
+}
+
+// Task 3
+let NamesArray = ["Mohammed", "Ahmad", "Ali", "Abdullah", "Omar"]
+
+for names in NamesArray {
+    print(names)
+}
+
+// Task 4
+var FruitsDict = ["Apple": 3, "Mango": 6, "Orange": 2, "Banana": 8]
+
+for (fname, quantity) in FruitsDict {
+    print("\(fname): \(quantity)")
+}
+
+// Task 5
+let name = "Mohammed M"
+print("Welcome \(name)!")
